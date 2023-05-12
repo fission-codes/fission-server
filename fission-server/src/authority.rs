75059926c2532efc82df90107ee9e810fb439088
Steven Vandevelde <icid.asset@gmail.com>
2023-05-12 13:18:26 +0200

    test: write tests

--- fission-server/src/authority.rs
+++ fission-server/src/authority.rs
@@ -4,10 +4,6 @@ use fission_common::authority::key_material::SUPPORTED_KEYS;
 use std::time::{SystemTime, UNIX_EPOCH};
 use ucan::crypto::did::DidParser;
 
-// 🧬
-
-use fission_common::authority;
-
 ///////////
 // TYPES //
 ///////////
@@ -25,7 +21,7 @@ pub struct Authority {
 
 impl Authority {
     /// Validate an authority struct
-    pub async fn validate(&self) -> Result<(), authority::Error> {
+    pub async fn validate(&self) -> Result<(), String> {
         let mut did_parser = DidParser::new(SUPPORTED_KEYS);
         let current_time = SystemTime::now()
             .duration_since(UNIX_EPOCH)
@@ -34,8 +30,35 @@ impl Authority {
 
         ucan::Ucan::validate(&self.ucan, current_time, &mut did_parser)
             .await
-            .map_err(|err| authority::Error::InvalidUcan {
-                reason: err.to_string(),
-            })
+            .map_err(|err| err.to_string())
+    }
+}
+
+///////////
+// TESTS //
+///////////
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use fission_common::authority::key_material::{generate_ed25519_material, SERVER_DID};
+    use ucan::builder::UcanBuilder;
+
+    #[tokio::test]
+    async fn validation_test() {
+        let issuer = generate_ed25519_material();
+        let ucan = UcanBuilder::default()
+            .issued_by(&issuer)
+            .for_audience(SERVER_DID)
+            .with_lifetime(100)
+            .build()
+            .unwrap()
+            .sign()
+            .await
+            .unwrap();
+
+        let authority = Authority { ucan };
+
+        assert_eq!(authority.validate().await.is_ok(), true);
     }
 }
