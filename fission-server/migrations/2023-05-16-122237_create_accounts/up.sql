33bb481b4316ade03f31f405398f208ed04f9596
Blaine Cook <romeda@gmail.com>
2023-07-04 11:17:22 -0700

    fix: correct schema

--- fission-server/migrations/2023-05-16-122237_create_accounts/up.sql
+++ fission-server/migrations/2023-05-16-122237_create_accounts/up.sql
@@ -3,7 +3,7 @@ CREATE TABLE accounts (
   did TEXT NOT NULL,
   username TEXT NOT NULL,
   email TEXT NOT NULL,
-  app_id SERIAL REFERENCES apps(id),
+  app_id INTEGER REFERENCES apps(id),
   inserted_at TIMESTAMP NOT NULL DEFAULT NOW(),
   updated_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
--- fission-server/src/db/schema.rs
+++ fission-server/src/db/schema.rs
@@ -6,6 +6,7 @@ diesel::table! {
         did -> Text,
         username -> Text,
         email -> Text,
+        app_id -> Nullable<Int4>,
         inserted_at -> Timestamp,
         updated_at -> Timestamp,
         volume_id -> Nullable<Int4>,
@@ -44,12 +45,6 @@ diesel::table! {
 }
 
 diesel::joinable!(accounts -> volumes (volume_id));
-diesel::joinable!(apps -> accounts (owner_id));
 diesel::joinable!(apps -> volumes (volume_id));
 
-diesel::allow_tables_to_appear_in_same_query!(
-    accounts,
-    apps,
-    email_verifications,
-    volumes,
-);
+diesel::allow_tables_to_appear_in_same_query!(accounts, apps, email_verifications, volumes,);
