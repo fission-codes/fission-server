6ea0f20bf08e033ba5b2e54be4beb91bb0cc8984
Blaine Cook <romeda@gmail.com>
2023-07-17 14:14:42 -0700

    chore: remove spurious logging

--- fission-server/src/db/schema.rs
+++ fission-server/src/db/schema.rs
@@ -47,4 +47,9 @@ diesel::joinable!(accounts -> volumes (volume_id));
 diesel::joinable!(apps -> accounts (owner_id));
 diesel::joinable!(apps -> volumes (volume_id));
 
-diesel::allow_tables_to_appear_in_same_query!(accounts, apps, email_verifications, volumes,);
+diesel::allow_tables_to_appear_in_same_query!(
+    accounts,
+    apps,
+    email_verifications,
+    volumes,
+);
