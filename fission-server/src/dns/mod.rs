144941db2db9001e66805cccd55bca590a228c32
Quinn Wilton <quinn@quinnwilton.com>
2023-07-21 16:25:41 -0700

    refactor: extract dns-over-https types into dns namespace

--- fission-server/src/dns/mod.rs
+++ fission-server/src/dns/mod.rs
@@ -1,2 +1,27 @@
 //! DNS
-pub mod handler;
+
+use anyhow::Result;
+use bytes::Bytes;
+use tokio::sync::broadcast;
+use trust_dns_server::server::{Request, RequestHandler};
+
+use self::{request_handler::Handler, response_handler::Handle};
+use crate::db::Pool;
+
+pub mod request_handler;
+pub mod response;
+pub mod response_handler;
+
+pub use self::response::Response;
+
+/// Handle a DNS request
+pub async fn handle_request(request: Request, db_pool: Pool) -> Result<Bytes> {
+    let (tx, mut rx) = broadcast::channel(1);
+    let response_handle = Handle(tx);
+
+    Handler::new(db_pool)
+        .handle_request(&request, response_handle)
+        .await;
+
+    rx.recv().await.map_err(|err| err.into())
+}
---
+++ fission-server/src/dns/response_handler.rs
@@ -0,0 +1,41 @@
+//! DNS Response Handler
+
+use async_trait::async_trait;
+use bytes::Bytes;
+use std::io;
+use tokio::sync::broadcast;
+use trust_dns_server::{
+    authority::MessageResponse,
+    proto::{self, serialize::binary::BinEncoder},
+    server::{ResponseHandler, ResponseInfo},
+};
+
+/// A handle to the channel over which the response to a DNS request will be sent
+#[derive(Debug, Clone)]
+pub struct Handle(pub broadcast::Sender<Bytes>);
+
+#[async_trait]
+impl ResponseHandler for Handle {
+    async fn send_response<'a>(
+        &mut self,
+        response: MessageResponse<
+            '_,
+            'a,
+            impl Iterator<Item = &'a proto::rr::Record> + Send + 'a,
+            impl Iterator<Item = &'a proto::rr::Record> + Send + 'a,
+            impl Iterator<Item = &'a proto::rr::Record> + Send + 'a,
+            impl Iterator<Item = &'a proto::rr::Record> + Send + 'a,
+        >,
+    ) -> io::Result<ResponseInfo> {
+        let mut bytes = Vec::with_capacity(512);
+        let info = {
+            let mut encoder = BinEncoder::new(&mut bytes);
+            response.destructive_emit(&mut encoder)?
+        };
+
+        let bytes = Bytes::from(bytes);
+        self.0.send(bytes).unwrap();
+
+        Ok(info)
+    }
+}
