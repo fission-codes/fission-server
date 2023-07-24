5e7440dc276d7455ed9903a1d8fc911188e2c5ba
Quinn Wilton <quinn@quinnwilton.com>
2023-07-24 15:30:21 -0700

    test: write tests for /healthcheck

--- fission-server/src/routes/health.rs
+++ fission-server/src/routes/health.rs
@@ -83,3 +83,96 @@ fn latest_embedded_migration_version() -> Option<String> {
         None
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use axum::{body::Body, http::Request};
+    use diesel::ExpressionMethods;
+    use diesel_async::RunQueryDsl;
+    use http::StatusCode;
+    use tower::ServiceExt;
+
+    use crate::{
+        db::__diesel_schema_migrations,
+        router::{setup_app_router, AppState},
+        test_utils::test_context::TestContext,
+    };
+
+    #[tokio::test]
+    async fn test_healthcheck_healthy() {
+        let ctx = TestContext::new();
+        let app_state = AppState {
+            db_pool: ctx.pool().await,
+        };
+
+        let app = setup_app_router(app_state);
+
+        let response = app
+            .oneshot(
+                Request::builder()
+                    .uri("/healthcheck")
+                    .body(Body::empty())
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+    }
+
+    #[tokio::test]
+    async fn test_healthcheck_db_unavailable() {
+        let ctx = TestContext::new();
+        let app_state = AppState {
+            db_pool: ctx.pool().await,
+        };
+
+        // Drop the database
+        drop(ctx);
+
+        let app = setup_app_router(app_state);
+
+        let response = app
+            .oneshot(
+                Request::builder()
+                    .uri("/healthcheck")
+                    .body(Body::empty())
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::SERVICE_UNAVAILABLE);
+    }
+
+    #[tokio::test]
+    async fn test_healthcheck_db_out_of_date() {
+        let ctx = TestContext::new();
+        let app_state = AppState {
+            db_pool: ctx.pool().await,
+        };
+
+        let mut conn = app_state.db_pool.get().await.unwrap();
+
+        // Insert a new migration at the end of time
+        diesel::insert_into(__diesel_schema_migrations::table)
+            .values(__diesel_schema_migrations::version.eq("2239-09-30-desolation".to_string()))
+            .execute(&mut conn)
+            .await
+            .unwrap();
+
+        let app = setup_app_router(app_state.clone());
+
+        let response = app
+            .oneshot(
+                Request::builder()
+                    .uri("/healthcheck")
+                    .body(Body::empty())
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::SERVICE_UNAVAILABLE);
+    }
+}
