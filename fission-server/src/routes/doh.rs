19ff01bb9a5ec85590c7f561c1c0497a7595539c
Quinn Wilton <quinn@quinnwilton.com>
2023-07-24 16:04:31 -0700

    test: add tests for querying dns records of SOA and gateway

--- fission-server/src/routes/doh.rs
+++ fission-server/src/routes/doh.rs
@@ -62,3 +62,115 @@ pub async fn post(
     )
         .into_response()
 }
+
+#[cfg(test)]
+mod tests {
+    use axum::{body::Body, extract::connect_info::MockConnectInfo, http::Request};
+    use axum_server::service::SendService;
+    use http::StatusCode;
+    use serde_json::{json, Value};
+    use std::net::SocketAddr;
+    use tower::ServiceExt;
+
+    use crate::{
+        router::{setup_app_router, AppState},
+        test_utils::test_context::TestContext,
+    };
+
+    use pretty_assertions::assert_eq;
+
+    #[tokio::test]
+    async fn test_dns_json() {
+        assert_dns_json(
+            "fission.app",
+            "soa",
+            json!(
+                {
+                  "Status": 0,
+                  "TC": false,
+                  "RD": false,
+                  "RA": false,
+                  "AD": false,
+                  "CD": false,
+                  "Question": [
+                    {
+                      "name": "fission.app.",
+                      "type": 6
+                    }
+                  ],
+                  "Answer": [
+                    {
+                      "name": "fission.app.",
+                      "type": 6,
+                      "TTL": 3600,
+                      "data": "dns1.fission.app. hostmaster.fission.codes. 2023000701 7200 3600 1209600 3600"
+                    }
+                  ],
+                  "Comment": null,
+                  "edns_client_subnet": null
+                }
+            ),
+        ).await;
+
+        assert_dns_json(
+            "gateway.fission.app",
+            "any",
+            json!(
+                {
+                  "Status": 0,
+                  "TC": false,
+                  "RD": false,
+                  "RA": false,
+                  "AD": false,
+                  "CD": false,
+                  "Question": [
+                    {
+                      "name": "gateway.fission.app.",
+                      "type": 255
+                    }
+                  ],
+                  "Answer": [
+                    {
+                      "name": "gateway.fission.app.",
+                      "type": 5,
+                      "TTL": 3600,
+                      "data": "prod-ipfs-gateway-1937066547.us-east-1.elb.amazonaws.com."
+                    }
+                  ],
+                  "Comment": null,
+                  "edns_client_subnet": null
+                }
+            ),
+        )
+        .await;
+    }
+
+    async fn assert_dns_json(name: &str, typ: &str, expected: Value) {
+        let ctx = TestContext::new();
+        let app_state = AppState {
+            db_pool: ctx.pool().await,
+        };
+
+        let app = setup_app_router(app_state)
+            .layer(MockConnectInfo(SocketAddr::from(([0, 0, 0, 0], 3000))))
+            .into_service();
+
+        let response = app
+            .oneshot(
+                Request::builder()
+                    .uri(format!("/dns-query?name={}&type={}", name, typ))
+                    .header("Accept", "application/dns-json")
+                    .body(Body::empty())
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
+        let body: Value = serde_json::from_slice(&body).unwrap();
+
+        assert_eq!(body, expected);
+    }
+}
