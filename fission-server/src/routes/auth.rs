2585d61625a400af05a3ee31725860826d90d38d
Philipp Krüger <philipp.krueger1@gmail.com>
2023-10-30 18:35:36 +0100

    chore: Write test for email code consumption

--- fission-server/src/routes/auth.rs
+++ fission-server/src/routes/auth.rs
@@ -73,7 +73,7 @@ mod tests {
         routes::auth::SuccessResponse,
         test_utils::{test_context::TestContext, RouteBuilder},
     };
-    use anyhow::anyhow;
+    use anyhow::{anyhow, Result};
     use assert_matches::assert_matches;
     use chrono::{Duration, Local};
     use diesel_async::RunQueryDsl;
@@ -82,24 +82,30 @@ mod tests {
     use serde_json::json;
     use testresult::TestResult;
 
-    #[test_log::test(tokio::test)]
-    async fn test_request_code_ok() -> TestResult {
-        let ctx = TestContext::new().await;
-
-        let email = "oedipa@trystero.com";
-
+    async fn request_code(email: &str, ctx: &TestContext) -> Result<(StatusCode, String, String)> {
         let (status, _) =
             RouteBuilder::<DefaultFact>::new(ctx.app(), Method::POST, "/api/v0/auth/email/verify")
                 .with_json_body(json!({ "email": email }))?
                 .into_json_response::<SuccessResponse>()
                 .await?;
 
-        let (email, _) = ctx
+        let (email_address, email_code) = ctx
             .verification_code_sender()
             .get_emails()
             .into_iter()
             .last()
             .expect("No email sent");
+
+        Ok((status, email_address, email_code))
+    }
+
+    #[test_log::test(tokio::test)]
+    async fn test_request_code_ok() -> TestResult {
+        let ctx = TestContext::new().await;
+
+        let email = "oedipa@trystero.com";
+
+        let (status, email, _) = request_code(email, &ctx).await?;
 
         assert_eq!(status, StatusCode::OK);
         assert_eq!(email, "oedipa@trystero.com");
@@ -113,17 +119,7 @@ mod tests {
 
         let email = "oedipa@trystero.com";
 
-        RouteBuilder::<DefaultFact>::new(ctx.app(), Method::POST, "/api/v0/auth/email/verify")
-            .with_json_body(json!({ "email": email }))?
-            .into_json_response::<SuccessResponse>()
-            .await?;
-
-        let (_, code) = ctx
-            .verification_code_sender()
-            .get_emails()
-            .into_iter()
-            .last()
-            .expect("No email sent");
+        let (_, _, code) = request_code(email, &ctx).await?;
 
         let token_result =
             EmailVerification::find_token(&mut ctx.get_db_conn().await, email, &code).await;
@@ -167,6 +163,28 @@ mod tests {
         let token_result = EmailVerification::find_token(&mut conn, email, code).await;
 
         assert_matches!(token_result, Err(_));
+
+        Ok(())
+    }
+
+    #[test_log::test(tokio::test)]
+    async fn test_request_code_consumed() -> TestResult {
+        let ctx = TestContext::new().await;
+        let conn = &mut ctx.get_db_conn().await;
+
+        let email = "oedipa@trystero.com";
+
+        let (_, _, code) = request_code(email, &ctx).await?;
+
+        let token = EmailVerification::find_token(conn, email, &code).await?;
+
+        assert_eq!(&token.email, email);
+
+        token.consume_token(conn).await?;
+
+        let result = EmailVerification::find_token(conn, email, &code).await;
+
+        assert_matches!(result, Err(_));
 
         Ok(())
     }
