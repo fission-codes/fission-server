7b65b938783fed20cd233af9633ad003ccb6f480
Philipp Krüger <philipp.krueger1@gmail.com>
2023-11-27 20:49:47 +0100

    chore: Fix format

--- fission-server/src/authority.rs
+++ fission-server/src/authority.rs
@@ -101,8 +101,8 @@ impl<F: Clone + DeserializeOwned> Authority<F> {
         caps.first()
             .ok_or_else(|| {
                 anyhow!(
-                "Invalid authorization. Couldn't find proof for {ability_str} as issued from {did}"
-            )
+                    "Invalid authorization. Couldn't find proof for {ability_str} as issued from {did}"
+                )
             })?
             .resource()
             .downcast_ref()
